2d036808bac6b254a8700e94da9cdc550f5c4535
Katherine Hinrichs <ko.hinrichs@gmail.com>
2021-02-26 15:34:42 -0600

    place-names and enemies-list

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,37 @@
 // Put your code here
+let enemies = [
+    {
+        firstName: "Joshua",
+        lastName: "Flowers",
+        offenses: ["Being a jerk to me in elementary school", "Not being nice to me in elementary school"],
+        isReallyHated: true
+    },
+    {
+        firstName: "Darth",
+        lastName: "Vader",
+        offenses: ["Cut off Luke's hand", "Murdered all those kids", "Unkind management practices"],
+        isReallyHated: false
+    },
+    {
+        firstName: "Betty",
+        lastName: "Rudelady",
+        offenses: ["Phone calls in the theater", "Phone calls on the bus", "Phone calls in line at the grocery store", "Poor conversationalist"],
+        isReallyHated: true
+    },
+    {
+        firstName: "Leon",
+        lastName: "Peck",
+        offenses: ["Keeps giving me a hotplate"],
+        isReallyHated: false
+    }
+
+]
+
+let enemyList = enemies.map(e => {
+    if (e.isReallyHated === true) {
+        return `${e.firstName} ${e.lastName} (Really, really dislike!)`
+    } else {
+        return `${e.firstName} ${e.lastName}`
+    }
+})
+console.log("My Enemies List", "----------------", enemyList)
--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,9 @@
 // Put your code here
+let places = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+console.log("All Place Names", places)
+
+let placesWithThe = places.filter(p => p.startsWith("The"))
+
+console.log("'The' Place Names", placesWithThe)
