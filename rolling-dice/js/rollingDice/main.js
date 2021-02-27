61a1ff79cee79bf9b4dbe153b31f386a243d1b8a
Katherine Hinrichs <ko.hinrichs@gmail.com>
2021-02-27 11:49:15 -0600

    Rolled dice.

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,24 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!")
+// Function to "roll" the dice
+let roll = () => {
+    min = Math.ceil(1);
+    max = Math.floor(6);
+    let dieValue = Math.floor(Math.random() * (6 - 1 + 1) + 1)
+
+    return dieValue
+}
+
+for (i = 0; i < 10; i++) {
+    let die1 = roll();
+    let die2 = roll();
+
+    let message = `${die1} + ${die2} = ${die1 + die2}`
+    if (die1 === die2) {
+        message += " DOUBLES!"
+    }
+    console.log(message)
+}
+
+
